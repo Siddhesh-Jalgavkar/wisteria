1306ae11c6fcf1189d034600eabf705958a72c51
Siddhesh <siddheshjalgavkar66@gmail.com>
2024-04-15 16:39:20 +0530

    Titel change

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-<title>Place icons/anchor tags/elemenst around circular container</title>
+<title>Wisteria</title>
 <!--[if lt IE 9]>
 	<script src="dist/html5shiv.js"></script>
 <![endif]-->
